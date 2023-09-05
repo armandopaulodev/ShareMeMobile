3f9bbfb1f5cd5065f25b95c2f977c8bd827d1afd
Armando Paulo <51958179+armandopaulodev@users.noreply.github.com>
2023-09-04 18:02:15 -0700

    sharing an dowloding converted file

--- src/pages/conversor/Conversor.js
+++ src/pages/conversor/Conversor.js
@@ -1,7 +1,7 @@
 
 import * as DocumentPicker from 'expo-document-picker';
 import { Camera, FileArchive } from 'lucide-react-native';
-import React, { useState,useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View } from "react-native";
 import { Fold, Wave } from 'react-native-animated-spinkit';
 import { Button, Text } from "react-native-paper";
@@ -10,6 +10,8 @@ import ConvertionService from '../../services/conversor/ConvertionService';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
+import * as Sharing from 'expo-sharing';
 
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
@@ -27,39 +29,48 @@ export default function Conversor({ navigation }) {
 
     const { toggleThemeType, themeType, isDarkTheme, theme } = useTheme();
     const [pickedDocument, setPickedDocument] = useState(null);
-    const [converting, setConverting]=useState(false);
+    const [converting, setConverting] = useState(false);
 
     const delay = ms => new Promise(
         resolve => setTimeout(resolve, ms)
     );
 
     const pickDocument = async () => {
-        
+
         try {
             const result = await DocumentPicker.getDocumentAsync({
-                type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 
+                type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
             });
             if (result.canceled === false) {
                 setConverting(true); //enable spinner if picked
                 setPickedDocument(result.assets[0]);
-                 ConvertionService.wordToPdf(result.assets[0]).then((response)=>{
+                ConvertionService.wordToPdf(result.assets[0]).then((response) => {
                     if (response.status === 200) {
-                         schedulePushNotification(); //local notification
-                         delay(4000).then(()=>{
+                        schedulePushNotification(); //local notification
+                        delay(4000).then(() => {
                             setConverting(false) //stop spinner
                             FileSystem.downloadAsync(
                                 response.data.url,
                                 FileSystem.documentDirectory + 'small.pdf'
-                              )
-                                .then(({ uri }) => {
-                                  console.log('Finished downloading to ', uri);
+                            )
+                                .then(({ uri }) =>  {
+                                    console.log('Finished downloading to ', uri);
+                                    // Saving the file in a folder name `MyImages`
+                                    const { status } =  MediaLibrary.getPermissionsAsync();
+                                    if (status === "granted") {
+                                        const asset = MediaLibrary.createAssetAsync(url)
+                                        MediaLibrary.createAlbumAsync("MyImages", asset, false)
+                                    }
+
+                                    // Sharing the downloded file
+                                    Sharing.shareAsync(uri);
                                 })
                                 .catch(error => {
-                                  console.error(error);
+                                    console.error(error);
                                 });
-                         })
-                      }
-                 })         
+                        })
+                    }
+                })
             } else {
                 console.log('Document picking canceled.');
             }
@@ -87,32 +98,32 @@ export default function Conversor({ navigation }) {
 
     return (
         <View style={{ justifyContent: 'center', padding: 10, marginLeft: 10 }}>
-           
+
             <Button mode="contained" onPress={() => pickDocument()}>
                 Carregar documento (.docx)
             </Button>
 
             {pickedDocument && (
-                <View style={{ marginTop:10, justifyContent:'space-between' }}>
-                    <Text style={{ fontWeight:'bold' }}>
-                    <FileArchive color={theme.colors.secondary} size={48} />
-                    {pickedDocument.name}
+                <View style={{ marginTop: 10, justifyContent: 'space-between' }}>
+                    <Text style={{ fontWeight: 'bold' }}>
+                        <FileArchive color={theme.colors.secondary} size={48} />
+                        {pickedDocument.name}
                     </Text>
-                    
+
                 </View>
             )}
 
             {
-                converting? <>
-                <Text style={{ textAlign: 'center', marginTop: 100 }} onPress={toggleThemeType}>
-                    <Wave size={200} color={theme.colors.primary} />
-                </Text>
-                <Text style={{ textAlign: 'center', marginTop: 20 }} onPress={toggleThemeType}>
-                    Convertendo! Por favor Aguarde...
-                </Text>
-                </> :''
+                converting ? <>
+                    <Text style={{ textAlign: 'center', marginTop: 100 }} onPress={toggleThemeType}>
+                        <Wave size={200} color={theme.colors.primary} />
+                    </Text>
+                    <Text style={{ textAlign: 'center', marginTop: 20 }} onPress={toggleThemeType}>
+                        Convertendo! Por favor Aguarde...
+                    </Text>
+                </> : ''
             }
-          
+
         </View>
     );
 };
