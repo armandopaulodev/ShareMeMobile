cf45b367e5fcae9f0a62b5bb8ddae8add1882b09
Armando Paulo <51958179+armandopaulodev@users.noreply.github.com>
2023-09-04 19:16:28 -0700

    animatable viw add

--- src/pages/rooms/Classroom.js
+++ src/pages/rooms/Classroom.js
@@ -3,15 +3,20 @@ import React from "react";
 import { View } from "react-native";
 import { Text, Button } from "react-native-paper";
 import { useTheme } from "../../context/ThemeContext";
+import * as Animatable from 'react-native-animatable';
 
 export default function Classroom({navigation}) {
     const { toggleThemeType, themeType, isDarkTheme, theme } = useTheme();
  
     return (
-        <View style={{ flex: 1, justifyContent: 'center', padding: 35, top:-50 }}>
-            <Button mode="contained">
+        <View style={{ justifyContent: 'center', padding: 35 }}>
+           
+            <Animatable.View animation='slideInLeft' easing={'ease-in-out-quad'} iterationCount={3} direction="alternate">
+            <Button mode="contained" icon={'account-group'} style={{ padding:10 }}>
                 Carregar documento (.docx)
             </Button>
+            </Animatable.View>
+
         </View>
     );
 };
