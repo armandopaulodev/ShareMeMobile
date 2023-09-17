f4348d2fcfb6196963adcc0983c88ddeab3b284f
Armando Paulo <51958179+armandopaulodev@users.noreply.github.com>
2023-09-18 01:31:09 +0200

    ...

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -1,22 +1,52 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
+import { View, Animated, Dimensions, StyleSheet, Alert, Linking } from 'react-native';
 import { Text } from "react-native-paper";
-import { Actionsheet, ActionsheetBackdrop, ActionsheetDragIndicator, ActionsheetContent, ActionsheetDragIndicatorWrapper, Pressable, GluestackUIProvider, Button, ButtonText } from '@gluestack-ui/themed';
+import { Image, GluestackUIProvider } from '@gluestack-ui/themed';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
+
+const { width, height } = Dimensions.get('screen');
+
+const ITEM_WIDTH = width;
+const ITEM_HEIGHT = height * .30;
 
 export default function Details({ route, navigation }) {
 
     const [param, setParams] = useState(route.params);
+
+      // variables bottom sheet
+      const snapPoints = useMemo(() => ['45%', '80%'], []);
+
+    //Spinner
+
+    const [loader, setLoading] = useState(false);
     const [showActionsheet, setShowActionsheet] = React.useState(true)
-    const handleClose = () => setShowActionsheet(!showActionsheet)
+    const handleClose = () => setShowActionsheet(true);
 
+    const loadSpinner = () => {
+        setLoading(!loader);
+    }
     return (<GluestackUIProvider>
-        <Actionsheet isOpen={showActionsheet} onClose={handleClose}>
-            <ActionsheetBackdrop />
-            <ActionsheetContent maxHeight="75%">
-                <ActionsheetDragIndicatorWrapper>
-                    <ActionsheetDragIndicator />
-                </ActionsheetDragIndicatorWrapper>
+        <View style={{ backgroundColor:'#cbd5e1' }}>
+        <View>
+            <Image source={param.imageUrl} style={styles.imagens} alt="imoveis"
+                onLoadStart={() => loadSpinner()} onLoadEnd={() => loadSpinner()} />
+        </View>
+        <BottomSheet snapPoints={snapPoints}>
+            <BottomSheetScrollView>
 
-            </ActionsheetContent>
-        </Actionsheet>
+
+            </BottomSheetScrollView>
+        </BottomSheet>
+        </View>
+
     </GluestackUIProvider>)
 }
+
+
+const styles = StyleSheet.create({
+    imagens: {
+        width: ITEM_WIDTH,
+        height: ITEM_HEIGHT,
+        resizeMode: 'cover'
+    }
+})
